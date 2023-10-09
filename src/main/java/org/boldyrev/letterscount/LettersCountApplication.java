b5c85bb93dc3fa3ce54ccc451756ca8a1d342d85
Alex Boldyrev <alexandrboldyrev08@gmail.com>
2023-10-09 21:44:20 +0300

    feat: add project structure

---
+++ src/main/java/org/boldyrev/letterscount/LettersCountApplication.java
@@ -0,0 +1,13 @@
+package org.boldyrev.letterscount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettersCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LettersCountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/boldyrev/letterscount/LettersCountApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boldyrev.letterscount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettersCountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
